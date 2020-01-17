5c0a305cd6ec84766469b6b578d4eccce79953e8
owenltm <owenlwiantoro01@gmail.com>
2020-01-17 21:02:33 +0700

    end of part5

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,5 @@ Route::get('/users/{id}/{name}', function($id, $name){
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostController');
