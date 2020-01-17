2e5da65b5339ce0c86b74fd5fda27e66747a7ec4
owenltm <owenlwiantoro01@gmail.com>
2020-01-17 19:09:53 +0700

    end of part4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-        <title>LSAPP</title>
+    <title>{{config('app.name', 'LSAPP')}}</title>
 
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar');
+
+
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section("content")
-<h1>{{$title}}</h1>
-<p>This is the laravel application</p>
+
+<div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application</p>
+    <a class="btn btn-primary" href="/login" role="button">Login</a>
+    <a class="btn btn-success" href="/register" role="button">Register</a>
+</div>
+
+
 @endsection
