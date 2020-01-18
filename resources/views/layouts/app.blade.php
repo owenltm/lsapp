9a1fa592d2a2c470d02ace2868699773c876a8fa
owenltm <owenlwiantoro01@gmail.com>
2020-01-18 16:24:39 +0700

    end of part9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,36 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    </head>
-    <body>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
         @include('inc.navbar')
-        <div class="container">
-            @include('inc.messages')
-            @yield('content')
-        </div>
 
-        <script src="//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
-        <script>
-            CKEDITOR.replace( 'body-ckeditor' );
-        </script>
-    </body>
+        <main class="py-4 container">
+            @yield('content')
+        </main>
+    </div>
+
+    <script src="//cdn.ckeditor.com/4.13.1/standard/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'body-ckeditor' );
+    </script>
+</body>
 </html>
